a252d043835ba2f23efddc8dd45078148b4df59b
Andrew Rueckert <ruke47@users.noreply.github.com>
2024-12-07 14:54:10 -0800

    Day 07.2

--- d07/src/main.rs
+++ d07/src/main.rs
@@ -1,8 +1,8 @@
 use std::fs;
 
 struct Equation {
-    result: i64,
-    values: Vec<i64>
+    result: u64,
+    values: Vec<u64>
 }
 
 fn main() {
@@ -12,10 +12,9 @@ fn main() {
 
 fn part1() {
     let equations = load_equations();
-    let sum: i64 = equations.iter()
+    let sum: u64 = equations.iter()
         .filter(|equation| {
-            let possible_results = get_possible_results(&equation.values);
-            // println!("{:?}", possible_results);
+            let possible_results = get_possible_results(&equation.values, false);
             return possible_results.contains(&equation.result)
         })
         .map(|equation| equation.result)
@@ -25,17 +24,34 @@ fn part1() {
 }
 
 fn part2() {
+    let equations = load_equations();
+    let sum: u64 = equations.iter()
+        .filter(|equation| {
+            let possible_results = get_possible_results(&equation.values, true);
+            return possible_results.contains(&equation.result)
+        })
+        .map(|equation| equation.result)
+        .sum();
 
+    println!("Part 2: {sum}");
 }
 
-fn get_possible_results(values: &[i64]) -> Vec<i64> {
+fn get_possible_results(values: &[u64], allow_concat: bool) -> Vec<u64> {
     let (last, rest) = values.split_last().unwrap();
     if rest.is_empty() {
         vec![*last]
     } else {
-        get_possible_results(rest).into_iter()
+        get_possible_results(rest, allow_concat).into_iter()
             .flat_map(|downstream_result| {
-                [last + downstream_result, last * downstream_result].into_iter()
+                if allow_concat {
+                    let mut concatted = downstream_result.to_string();
+                    concatted.push_str(&last.to_string());
+                    let concatted: u64 = concatted.parse().unwrap();
+
+                    vec![last + downstream_result, last * downstream_result, concatted].into_iter()
+                } else {
+                    vec![last + downstream_result, last * downstream_result].into_iter()
+                }
             })
             .collect()
     }
