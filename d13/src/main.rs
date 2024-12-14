b9d395c2e2d5a54f1baf028dd78799e97acc3f43
Andrew Rueckert <ruke47@users.noreply.github.com>
2024-12-13 20:55:50 -0800

    Day 13 part 2
    fixed my algebra

--- d13/src/main.rs
+++ d13/src/main.rs
@@ -1,17 +1,20 @@
 use lazy_static::lazy_static;
 use lib2d::Point2d;
-use num::integer::Roots;
 use regex::Regex;
 use std::fs;
 
 lazy_static! {
-    pub static ref MACHINE_DEF: Regex = Regex::new(r"Button A: X\+(?<ax>\d+), Y\+(?<ay>\d+)
+    pub static ref MACHINE_DEF: Regex = Regex::new(
+        r"Button A: X\+(?<ax>\d+), Y\+(?<ay>\d+)
 Button B: X\+(?<bx>\d+), Y\+(?<by>\d+)
-Prize: X=(?<px>\d+), Y=(?<py>\d+)").unwrap();
+Prize: X=(?<px>\d+), Y=(?<py>\d+)"
+    )
+    .unwrap();
 }
 
-type Point = Point2d<i128>;
+type Point = Point2d<i64>;
 
+#[derive(Debug)]
 struct Machine {
     a: Point,
     b: Point,
@@ -26,70 +29,55 @@ fn main() {
 fn part1() {
     let machines = read_machines(0);
     println!("Got {:?} machines", machines.len());
-    let cost: usize = machines.iter()
+    let cost: usize = machines
+        .iter()
         .map(get_working_counts)
-        .flat_map(|list| list.iter()
-            .map(|(a, b)| a * 3 + b * 1)
-            .min())
+        .flat_map(|list| list.iter().map(|(a, b)| a * 3 + b * 1).min())
         .sum();
 
     println!("Part 1: {cost}");
 }
 
 fn part2() {
-    let big_time: i128 = 0;
+    let big_time: i64 = 10000000000000;
     let machines = read_machines(big_time);
 
-    let cost: i128 = machines.iter()
+    let cost: i64 = machines
+        .iter()
         .flat_map(|machine| {
-            // we're just using the quadratic formula
-            let qa = machine.a.x * machine.a.x;
-            let qb = machine.a.x * machine.prize.y;
-            let qc = machine.prize.x * machine.b.y;
+            // wait a minute, this is just ALGEBRA!
+            let numerator = (machine.prize.x * machine.b.y) - (machine.b.x * machine.prize.y);
+            let denominator = (machine.a.x * machine.b.y) - (machine.b.x * machine.a.y);
 
-            // b^2 - 4ac
-            let sqrt_part = (qb * qb) - (4 * qa * qc);
-
-            // can't take the square root of a negative
-            if sqrt_part < 0 {
-                return None
-            }
-
-            // return early if sqrt(b^2 - 4ac) isn't an integer
-            let root = sqrt_part.sqrt();
-            if root * root != sqrt_part {
+            if numerator % denominator != 0 {
                 return None;
             }
 
-            // -b +/- sqrt(b^2 - 4ac)
-            let numerators = [1 * qb + root, -1 * qb - root];
-            let denominator = 2 * qa;
+            let a_count = numerator / denominator;
 
-            numerators.into_iter()
-                .flat_map(|numerator| {
-                    if numerator < 0 {
-                        None
-                    } else if numerator % denominator == 0 {
-                        Some(numerator / denominator)
-                    } else {
-                        None
-                    }
-                })
-                .flat_map(|a_count| {
-                    let b_count = (machine.prize.x - (machine.a.x * a_count)) / machine.b.x;
-                    if b_count < 0 {
-                        return None
-                    }
-                    let sum_point = (machine.a * a_count) + (machine.b * b_count);
-                    if sum_point != machine.prize {
-                        panic!("I did math bad! Expected {:?} but got {:?} with A={a_count} B={b_count}",
-                               machine.prize, sum_point);
-                    }
-                    return Some((a_count, b_count))
-                })
-                .map(|(a_count, b_count)| a_count * 3 + b_count)
-                .min()
+            let b_numerator = machine.prize.x - (machine.a.x * a_count);
+
+            if b_numerator % machine.b.x != 0 {
+                // apparently this is fine
+                println!(
+                    "I got an a_count of {a_count}, but b_count was non integer! {:?}",
+                    machine
+                );
+                return None;
+            }
+
+            let b_count = b_numerator / machine.b.x;
+            let test_point = machine.a * a_count + machine.b * b_count;
+            if test_point != machine.prize {
+                panic!(
+                    "I thought A={a_count} B={b_count} would give me {:?}, but I got {:?}",
+                    machine.prize, test_point
+                );
+            }
+
+            Some((a_count, b_count))
         })
+        .map(|(a_count, b_count)| a_count * 3 + b_count)
         .sum();
 
     println!("Part 2: {cost}");
@@ -119,15 +107,16 @@ fn get_working_counts(machine: &Machine) -> Vec<(usize, usize)> {
     working_counts
 }
 
-fn read_machines(prize_offset: i128) -> Vec<Machine> {
-    MACHINE_DEF.captures_iter(&fs::read_to_string("d13/example").unwrap())
+fn read_machines(prize_offset: i64) -> Vec<Machine> {
+    MACHINE_DEF
+        .captures_iter(&fs::read_to_string("d13/input").unwrap())
         .map(|caps| {
             let ax = caps["ax"].parse().unwrap();
             let ay = caps["ay"].parse().unwrap();
             let bx = caps["bx"].parse().unwrap();
             let by = caps["by"].parse().unwrap();
-            let px: i128 = caps["px"].parse().unwrap();
-            let py: i128 = caps["py"].parse().unwrap();
+            let px: i64 = caps["px"].parse().unwrap();
+            let py: i64 = caps["py"].parse().unwrap();
 
             Machine {
                 a: Point2d::new(ax, ay),
