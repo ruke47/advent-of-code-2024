7c602ca91e8b02b7ddf1e4dd606607288606a212
Andrew Rueckert <ruke47@users.noreply.github.com>
2024-12-19 17:06:48 -0800

    Day 15.2

--- d15/src/main.rs
+++ d15/src/main.rs
@@ -4,21 +4,52 @@ use lib2d::{dir_delta, Direction, Point2d};
 use std::cell::RefCell;
 use std::collections::HashSet;
 use std::fs;
+use std::hash::{Hash, Hasher};
 
 type Coord = i32;
 type Point = Point2d<Coord>;
 
+#[derive(Eq, PartialEq, Debug)]
 struct SlidyCrate {
+    id: usize,
     loc: RefCell<Point>,
+    width: Coord,
 }
+
+impl Hash for SlidyCrate {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        state.write_usize(self.id)
+    }
+}
+
 
 impl SlidyCrate {
     fn gps(&self) -> Coord {
         let point = self.loc.borrow();
         point.x + (point.y * 100)
     }
+
+    fn blocks(&self, point: &Point) -> bool {
+        let loc = self.loc.borrow();
+        let x_range = loc.x..(loc.x + self.width);
+
+        x_range.contains(&point.x) && loc.y == point.y
+    }
+
+    fn points(&self) -> Vec<Point> {
+        let loc = self.loc.borrow();
+        (0..self.width)
+            .map(|dx| Point2d::new(loc.x + dx, loc.y))
+            .collect()
+    }
+
+    fn shift(&self, direction: &Direction) {
+        let destination = *self.loc.borrow() + dir_delta(*direction);
+        self.loc.replace(destination);
+    }
 }
 
+#[derive(Debug)]
 struct Board {
     walls: HashSet<Point>,
     boxes: Vec<SlidyCrate>,
@@ -34,40 +65,38 @@ impl Board {
 
         let instr = self.instructions.pop().unwrap();
         let bot = &self.bot;
-        self.try_move(bot, &instr);
+        let mut impacted_boxes = HashSet::new();
+        if self.could_move(bot, &instr, &mut impacted_boxes) {
+            impacted_boxes.iter().for_each(|b| {
+                b.shift(&instr);
+            })
+        }
         true
     }
 
-    fn try_move(&self, mover: &SlidyCrate, instr: &Direction) -> bool {
-        let destination = *mover.loc.borrow() + dir_delta(*instr);
+    fn could_move<'a>(&'a self, mover: &'a SlidyCrate, instr: &Direction,
+                  impacted_boxes: &mut HashSet<&'a SlidyCrate>) -> bool {
+        impacted_boxes.insert(mover);
+
+        let destinations = mover.points().into_iter()
+            .map(|point| point + dir_delta(*instr))
+            .collect_vec();
 
         // can't negotiate with a wall
-        if self.walls.contains(&destination) {
+        if destinations.iter().any(|destination| self.walls.contains(destination)) {
             return false;
         }
 
-        // figure out if some box is in our way
-        let blocker = self.boxes.iter()
-            .find(|b| *b.loc.borrow() == destination);
+        let blockers = self.boxes.iter()
+            // figure out if some box is in our way
+            .filter(|b| {
+                destinations.iter().any(|destination| b.blocks(destination))
+            })
+            // but it's fine if we slide into another point that we contain
+            .filter(|b| *b != mover)
+            .collect_vec();
 
-        match blocker {
-            Some(blocker_crate) => {
-                // if there's a blocker...
-                if self.try_move(blocker_crate, instr) {
-                    // if the blocker can be moved, move into their place
-                    mover.loc.replace(destination);
-                    true
-                } else {
-                    // if the blocker cannot be moved, we cannot be moved
-                    false
-                }
-            },
-            None => {
-                // if there's no blocker, we can move into the free space
-                mover.loc.replace(destination);
-                true
-            }
-        }
+        blockers.iter().all(|b| self.could_move(b, instr, impacted_boxes))
     }
 }
 
@@ -77,23 +106,32 @@ fn main() {
 }
 
 fn part1() {
-    let mut board = load_board();
+    let mut board = load_board(1);
     loop {
         if !board.tick() {
-            break
+            break;
         }
     }
 
-    let sum: Coord = board.boxes.iter()
-        .map(|b| b.gps())
-        .sum();
+    let sum: Coord = board.boxes.iter().map(|b| b.gps()).sum();
 
     println!("Part 1: {sum}");
 }
 
-fn part2() {}
+fn part2() {
+    let mut board = load_board(2);
+    loop {
+        if !board.tick() {
+            break;
+        }
+    }
 
-fn load_board() -> Board {
+    let sum: Coord = board.boxes.iter().map(|b| b.gps()).sum();
+
+    println!("Part 2: {sum}");
+}
+
+fn load_board(width: Coord) -> Board {
     let mut walls = HashSet::new();
     let mut boxes = Vec::new();
     let mut bot: Option<Point> = None;
@@ -104,31 +142,42 @@ fn load_board() -> Board {
         .map(|p| p.to_string())
         .collect_tuple()
         .unwrap();
-
-    map_str.lines()
-        .enumerate()
-        .for_each(|(y, line)| {
-            line.chars().enumerate()
-                .for_each(|(x, char)| {
-                    let point = Point2d::new(x as Coord, y as Coord);
-                    match char {
-                        '#' => { walls.insert(point); }
-                        'O' => boxes.push(SlidyCrate {loc: RefCell::new(point) } ),
-                        '@' => bot = Some(point),
-                        _ => {}
+    let mut crate_id = 1;
+    map_str.lines().enumerate().for_each(|(y, line)| {
+        line.chars().enumerate().for_each(|(x, char)| {
+            let point = Point2d::new(
+                width * x as Coord,
+                y as Coord
+            );
+            match char {
+                '#' => {
+                    for i in 0..width {
+                        let offset_point = Point2d::new(i, 0) + point;
+                        walls.insert(offset_point);
                     }
-                })
-        });
-
-    let moves = instruction_str.chars()
-        .flat_map(|dir| {
-            match dir {
-                '^' => Some(Up),
-                'v' => Some(Down),
-                '>' => Some(Right),
-                '<' => Some(Left),
-                _ => None
+                }
+                'O' => {
+                    boxes.push(SlidyCrate {
+                        id: crate_id,
+                        loc: RefCell::new(point),
+                        width
+                    });
+                    crate_id += 1;
+                },
+                '@' => bot = Some(point),
+                _ => {}
             }
+        })
+    });
+
+    let moves = instruction_str
+        .chars()
+        .flat_map(|dir| match dir {
+            '^' => Some(Up),
+            'v' => Some(Down),
+            '>' => Some(Right),
+            '<' => Some(Left),
+            _ => None,
         })
         // reverse so we can pop
         .rev()
@@ -137,7 +186,11 @@ fn load_board() -> Board {
     Board {
         walls,
         boxes,
-        bot: SlidyCrate {loc: RefCell::new(bot.unwrap()) },
-        instructions: moves
+        bot: SlidyCrate {
+            id: 0,
+            loc: RefCell::new(bot.unwrap()),
+            width: 1
+        },
+        instructions: moves,
     }
 }
